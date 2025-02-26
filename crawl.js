f32538bf16cef1eaf11f8c620600324c59217994
Justus <justus.zimmermann@gmx.de>
2025-02-26 16:49:52 +0100

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,26 @@
 const {JSDOM} = require('jsdom')
 
+async function crawlPage(currentURL){
+    console.log(`actively crawling ${currentURL}`);
+    try{
+        const res = await fetch(currentURL);
+        if(res.status>399){
+            console.log(`Error when fetching with status code: ${res.status}`)
+            return
+        }
+        const contentType = res.headers.get('content-type')
+        if(!contentType.includes("text/html")){
+            console.log("No html response.Instead: ", contentType)
+            return
+        }
+        const body = await res.text();
+        console.log(body)
+    }catch(err){
+        console.log(`Error when fetching ${currentURL}. Exit with Error: ${err}`)
+    }
+    
+}
+
 function normalizeURL(urlString){
     const urlObj = new URL(urlString);
     const hostpath = `${urlObj.hostname}${urlObj.pathname}`
@@ -41,5 +62,6 @@ function getURLsFromHTML(htmlBody, baseURL){
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
