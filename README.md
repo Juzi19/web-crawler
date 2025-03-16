6a3810b7c5d4e1fd4daf96801541b68540e9548c
Justus <justus.zimmermann@gmx.de>
2025-03-16 16:46:36 +0100

    prompt imrpoved & update readme

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 ### Description
 
 A simple web crawler that can summarize a websites text (e.g. p tags) and can extract contact information (based on a simple algorithm).
-The webcrawler is a console application, allowing users to input an URL and add the scraping mode (contact, etc..). The application will then scrape the page and print the results in the console (e.g. phone numbers, email, social media accounts)
+The webcrawler is a console application, allowing users to input an URL and add the scraping mode (contact, etc..). The application will then scrape the page and print the results in the console (e.g. phone numbers, email, social media accounts). Additionally, it provides access to the openai api, enabling smart website summaries
+
+
+
+
--- summarize.js
+++ summarize.js
@@ -1,19 +1,21 @@
 // Summarizing pages content and providing a written report for the link structure
 
 const {OpenAI} = require('openai');
+const { printContactData } = require('./report');
 require('dotenv').config();
 const api_key = process.env.OPEN_AI;
 
 async function summarize_report(report, pages){
+    const contact_links = printContactData(pages)
     const client = new OpenAI({apiKey:api_key})
     const response = await client.chat.completions.create({
         model: "gpt-4o",
         messages: [
-          { role: "system", content: "Gib deine Antwort IMMER als JSON-Objekt mit folgendem Schema zurück: {'linking_structure': 'Kurzer Überblick über die Link Struktur (ca. 20 Wörter) mit kurzer Erklärung', 'content': 'Überblick über Website Inhalt (80 Wörter)'}" },
+          { role: "system", content: "Gib deine Antwort IMMER als JSON-Objekt mit folgendem Schema zurück: {'linking_structure': 'Kurzer Überblick über die Link Struktur (ca. 30 Wörter) mit kurzer Erklärung', 'content': 'Überblick über Website Inhalt (70 Wörter)'}, 'contact': 40 Wörter die Kontaktwege erklären" },
           // es wird nur der Text der Website als input übergeben
-          { role: "user", content: `Link Struktur: ${pages}; Website Inhalt: ${ report[0][1].text}`},
+          { role: "user", content: `Link Struktur: ${pages}; Website Inhalt: ${ report[0][1].text}, Kontakt: ${contact_links}`},
         ],
-        max_tokens: 700
+        max_tokens: 800
       });
       return response.choices[0].message.content;
       
